176f1b67db98e0c7a634d6234ac2a52575f8600d
bic_lighter81 <moritzben.becker@outlook.de>
2025-01-09 19:20:24 +0100

    feat(ui): adds rnr component library

---
+++ apps/native/app/index.tsx
@@ -0,0 +1,7 @@
+import { Button } from '~/components/ui/button';
+import { Text } from '~/components/ui/text';
+
+export default function Page() {
+    return <Button><Text>Click me</Text></Button>
+
+}
